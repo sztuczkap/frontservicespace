2668438aa102c2f8bbb5e0f4af56833209b8c037
Piotr <p.sztuczka@wp.pl>
2023-08-11 16:36:00 +0200

    initialize app and add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ServiceComponent} from './service/service.component';
+import {EmployeeComponent} from './employee/employee.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ClientComponent} from './client/client.component';
+import {OrderComponent} from './order/order.component';
+import {NewOrderComponent} from './new-order/new-order.component';
+
+const appRoute: Routes = [
+  // {path: '', component: ServiceComponent},
+  // {path: '', redirectTo: 'service', pathMatch: 'full'},
+  {path: 'new-order', component: NewOrderComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'client', component: ClientComponent},
+  {path: 'service', component: ServiceComponent},
+  {path: 'employee', component: EmployeeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    EmployeeComponent,
+    ClientComponent,
+    OrderComponent,
+    NewOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
